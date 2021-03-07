872b0dbdc920fcc06ff0e0206dff3921b2cfa51e
MatheusBrinatiAltomar <matheus.altomar@ice.ufjf.br>
2021-03-07 13:34:29 -0300

    Adiciona posição do sprite no mapa

--- js/Sprite.js
+++ js/Sprite.js
@@ -11,27 +11,39 @@ export default class Sprite
         this.w = w;
         this.h = h;
         this.color = color;
+        this.cena = null;
+        this.mapaX = 0;
+        this.mapaY = 0;
     }
 
     desenhar(ctx)
     {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x - this.w/2, this.y - this.h/2, this.w, this.h);
+        ctx.fillRect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
+        ctx.strokeStyle = "blue";
+        ctx.strokeRect(
+            this.mapaX * this.cena.mapa.SIZE,
+            this.mapaY * this.cena.mapa.SIZE,
+            this.cena.mapa.SIZE,
+            this.cena.mapa.SIZE
+        );
     }
 
     passo(dt)
     {
-        this.x = this.x + this.vx*dt;
-        this.y = this.y + this.vy*dt;
+        this.x = this.x + this.vx * dt;
+        this.y = this.y + this.vy * dt;
+        this.mapaX = Math.floor(this.x / this.cena.mapa.SIZE);
+        this.mapaY = Math.floor(this.y / this.cena.mapa.SIZE);
     }
 
     colidiuCom(outro)
     {
         return !(
-            this.x - this.w/2 > outro.x + outro.w/2 ||
-            this.x + this.w/2 < outro.x - outro.w/2 ||
-            this.y - this.h/2 > outro.y + outro.h/2 ||
-            this.y + this.h/2 < outro.y - outro.h/2
+            this.x - this.w / 2 > outro.x + outro.w / 2 ||
+            this.x + this.w / 2 < outro.x - outro.w / 2 ||
+            this.y - this.h / 2 > outro.y + outro.h / 2 ||
+            this.y + this.h / 2 < outro.y - outro.h / 2
         );
     }
 }
